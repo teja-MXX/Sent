c746c4a4b5543dda94522b7ce62e3bf2c2989708
teja-MXX <teja.mandapakala@gmail.com>
2022-05-28 23:44:11 +0530

    Displayed Available Users in AddFriends Section

--- Application/addFriends.py
+++ Application/addFriends.py
@@ -1,10 +1,15 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, request
 from Application.models import db, User
 
 friendsBP = Blueprint('friendsBP', __name__)
 
-@friendsBP.route("/addFriends")
+@friendsBP.route("/addFriends", methods=["GET", "POST"])
 def friends():
+	print("YO MAN")
 	user = User.query.filter_by(UserName = session['uname']).first()
 	if user:
-		return render_template('friends.html', userDetails = user)
+		
+		friends = User.query.filter(User.UserName != session['uname']).all()
+		print("FCK")
+		print(friends)
+		return render_template('friends.html', userDetails = user, friendAccounts = friends)
