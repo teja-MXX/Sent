22e950d515a9ed82e817aff32ad95973efe16793
teja-MXX <teja.mandapakala@gmail.com>
2022-06-02 16:25:44 +0530

    CHAT - User Chat Window on clicking Search Results

--- Application/static/js/userSearch.js
+++ Application/static/js/userSearch.js
@@ -17,27 +17,26 @@ function ale(e){
   			.then(res => res.json())
   			.then(data => obj = data)
   			.then(function(){ 
-
-
-  				
   				for(let i=0; i<Object.keys(obj).length; i++){
-
-  					console.log(Object.values(obj)[i]['FirstName'])
   					searchResultsDiv = document.createElement('div')
   					searchResultsDiv.classList.toggle('searchResults')
 
   					pElement = document.createElement('p')
-  					console.log(Object.keys(obj)[i]['FirstName'])
   					pElement.innerText = Object.values(obj)[i]['FirstName'] + " " + Object.values(obj)[i]['LastName']
   					if(i === 0){
   						pElement.classList.toggle('firstResult')
   					}
   					searchResultsDiv.appendChild(pElement)
   					chatListDiv.append(searchResultsDiv)
+
+  					// Chat Select on clicking on Search Result
+  					searchResultss = document.querySelector(".searchResults")
+					UserFullName = document.getElementById('UserFullName')
+					searchResultss.addEventListener('click', function(){
+						UserFullName.innerText = searchResultss.firstChild.innerText
+					})
   				}
-
-  				
-
   			})
 	}
 }
+
